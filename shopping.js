8bb8c9c28846c46cae207eebe485d8c5eade773b
Marcus Baer <marcus.baer@greenpocket.de>
2013-05-08 10:13:28 +0200

    Initial

---
+++ shopping.js
@@ -0,0 +1,10 @@
+﻿var app = require('doxybox'),
+    config = require('./conf/config'),
+    service = require('./lib/service');
+
+app.config(config);
+app.service(service);
+app.static('static');
+app.jade('jade');
+
+app.start();
